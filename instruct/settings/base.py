b913cbcc259fd3458d6770f4964afd07ea96ffe4
leonardo1909 <leonardo.jose94@outlook.com>
2020-07-06 22:40:59 -0300

    Deploy Heroku.

--- instruct/settings/base.py
+++ instruct/settings/base.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,13 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'gm8x7r#xr2p^pe3ani2q3&prszg#(s!^^f2#v_ek8)6dd8i=p9'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
+SECRET_KEY = config('SECRET_KEY')
 
 # Application definition
 
--- manage.py
+++ manage.py
@@ -5,7 +5,8 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'instruct.settings')
+    # os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'instruct.settings.development')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'instruct.settings.production')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
